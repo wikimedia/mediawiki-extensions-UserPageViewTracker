a2d4e8d098e65dcbbb08e63212f96a9411c30f6b
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-01 16:47:42 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I2dc8efe350b554f8aaacc708eec72a3069bb5b62

--- UserPageViewTracker.php
+++ UserPageViewTracker.php
@@ -5,10 +5,11 @@ $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'UserPageViewTracker',
 	'author'=> array( 'Kimon Andreou', 'Luis Felipe Schenone' ),
 	'descriptionmsg' => 'userpageviewtracker-desc',
-	'version' => 0.3,
+	'version' => '0.4.0',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:UserPageViewTracker',
 );
 
+$wgMessagesDirs['UserPageViewTracker'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['UserPageViewTracker'] = __DIR__ . '/UserPageViewTracker.i18n.php';
 $wgExtensionMessagesFiles['UserPageViewTrackerAlias'] = __DIR__ . '/UserPageViewTracker.alias.php';
 
